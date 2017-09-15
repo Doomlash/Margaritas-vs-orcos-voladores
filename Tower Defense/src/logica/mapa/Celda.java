510a1a3445c1cb790874c93ec25866e1d9f7f1ef
Emi Vilche <31260215+Doomlash@users.noreply.github.com>
2017-09-15 01:43:32 -0300

    pequeñas cosas de celda
    2 metodos y ni siquiera completos

--- Tower Defense/src/logica/mapa/Celda.java
+++ Tower Defense/src/logica/mapa/Celda.java
@@ -1,8 +1,19 @@
 package logica.mapa;
 
 import java.util.List;
+
+import logica.disparo.Disparo;
 import logica.gameObjects.*;
+import logica.mapa.elementosMapa.destruibles.Piedra;
 
 public class Celda{
 	private List<Elemento> lista;
+
+	public void agregarElemento(Elemento e) {
+		lista.add(e);
+	}
+
+	public List<Elemento> getElementos() {
+		return lista;
+	}
 }
